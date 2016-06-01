3985f236907a8f501081aae55d20068b468cae23
Daniel Smith <dan@getabetterpic.com>
2016-05-31 20:08:09 -0400

    Add flight and motor associations to rocket

--- app/models/rocket.rb
+++ app/models/rocket.rb
@@ -1,3 +1,5 @@
 class Rocket < ActiveRecord::Base
   validates_presence_of :name
+  has_many :flights
+  has_many :motors, through: :flights
 end
--- spec/models/rocket_spec.rb
+++ spec/models/rocket_spec.rb
@@ -2,4 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Rocket, type: :model do
   it { should validate_presence_of :name }
+  it { should have_many :flights }
+  it { should have_many(:motors).through(:flights) }
 end

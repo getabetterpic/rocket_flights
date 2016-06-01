c7191e64fa921a329fcaffe6e9080d47d8b4fcdc
Daniel Smith <dan@getabetterpic.com>
2016-05-31 22:59:37 -0400

    Add successful_flights method

--- app/models/rocket.rb
+++ app/models/rocket.rb
@@ -2,4 +2,8 @@ class Rocket < ActiveRecord::Base
   validates_presence_of :name
   has_many :flights
   has_many :motors, through: :flights
+
+  def successful_flights
+    flights.where(successful: true)
+  end
 end
--- spec/models/rocket_spec.rb
+++ spec/models/rocket_spec.rb
@@ -4,4 +4,15 @@ RSpec.describe Rocket, type: :model do
   it { should validate_presence_of :name }
   it { should have_many :flights }
   it { should have_many(:motors).through(:flights) }
+
+  context '#successful_flights' do
+    let(:rocket) { FactoryGirl.create(:rocket) }
+    let(:motor) { FactoryGirl.create(:motor) }
+    let!(:flight_1) { FactoryGirl.create(:flight, successful: true, rocket: rocket, motor: motor) }
+    let!(:flight_2) { FactoryGirl.create(:flight, successful: false, rocket: rocket, motor: motor) }
+
+    it 'returns only successful flights' do
+      expect(rocket.successful_flights.count).to eq(1)
+    end
+  end
 end

6584233164782d965d78b44e5ed1a5d0e0f76bf0
Daniel Smith <dan@getabetterpic.com>
2016-06-02 07:57:33 -0400

    Update motor relationships with flight_motors

--- spec/models/motor_spec.rb
+++ spec/models/motor_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Motor, type: :model do
   it { should validate_presence_of :name }
   it { should validate_presence_of :diameter_in_mm }
-  it { should have_many :flights }
+  it { should have_many :flight_motors }
+  it { should have_many(:flights).through(:flight_motors) }
   it { should have_many(:rockets).through(:flights) }
 end

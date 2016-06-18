d05cab123c33c5c88bdd45d31d1cb7dcfdfc398c
getabetterpic <dan@danandjensmith.com>
2016-06-18 07:09:41 -0400

    Correct special character validations

--- app/models/motor.rb
+++ app/models/motor.rb
@@ -7,10 +7,12 @@ class Motor < ActiveRecord::Base
   before_validation :upcase_name
 
   validates_each :name do |record, attr, value|
-    if name =~ /[^A-Z0-9\-\/]*/
-      record.errors.add(:name, 'cannot contain any characters except A-Z, 0-9, -, and /')
+    if value =~ /[^A-Z0-9\-\/]+/
+      record.errors.add(attr, 'cannot contain any characters except A-Z, 0-9, -, and /')
     end
   end
+
+  private
 
   def upcase_name
     name.upcase! if name.present?
--- spec/models/motor_spec.rb
+++ spec/models/motor_spec.rb
@@ -8,13 +8,22 @@ RSpec.describe Motor, type: :model do
   it { should have_many(:flights).through(:flight_motors) }
   it { should have_many(:rockets).through(:flights) }
 
+  it 'successfully saves a correct motor' do
+    motor = FactoryGirl.build(:motor, name: 'A8-3')
+    expect(motor.save).to be true
+  end
+
   it 'upcases the motor name when saving' do
     motor = FactoryGirl.build(:motor, name: 'a8-3')
-    motor.save
+    motor.validate!
     expect(motor.name).to eq('A8-3')
   end
 
-  it 'validates the motor name does not contain any unwanted characters' do
-    expect { FactoryGirl.create(:motor, name: 'B5!-8') }.to raise_error(ActiveRecord::RecordInvalid)
+  %w(! @ # $ % ^ & * \( \) _ + = ? < > : ; \' \" { } [ ] | \\).each do |char|
+    it "validates the motor name does not contain an #{char}" do
+      expect { FactoryGirl.create(:motor, name: "B5#{char}-8") }.to raise_error(ActiveRecord::RecordInvalid)
+    end
   end
+
+
 end

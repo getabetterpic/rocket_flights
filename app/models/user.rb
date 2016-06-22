826d4e6d5bbb789d1a3919cadac86c92f60f475c
getabetterpic <dan@danandjensmith.com>
2016-06-22 11:04:50 -0400

    Add rockets to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
   validates_presence_of :email
   validates_uniqueness_of :email, scope: :auth0_user_id, case_sensitive: false
+  has_many :user_rockets
+  has_many :rockets, through: :user_rockets
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,5 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of :email }
   subject { FactoryGirl.build(:user) }
   it { should validate_uniqueness_of(:email).case_insensitive.scoped_to(:auth0_user_id) }
+  it { should have_many(:rockets).through(:user_rockets) }
 end

95d8dd5a53e9574a06bccbacffc5c8e6a5f570a9
getabetterpic <dan@danandjensmith.com>
2016-06-22 10:49:43 -0400

    Replace unique email index with just regular index

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160620140933) do
+ActiveRecord::Schema.define(version: 20160622144343) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,14 +67,13 @@ ActiveRecord::Schema.define(version: 20160620140933) do
   add_index "rockets", ["name", "manufacturer"], name: "index_rockets_on_name_and_manufacturer", unique: true, using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.citext   "email",           null: false
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.string   "password_digest"
+    t.citext   "email",         null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
     t.string   "auth0_user_id"
   end
 
   add_index "users", ["auth0_user_id", "email"], name: "index_users_on_auth0_user_id_and_email", unique: true, using: :btree
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", using: :btree
 
 end

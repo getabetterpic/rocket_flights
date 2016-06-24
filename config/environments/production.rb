f4438b0ef27193fd603a826987417e2a59e21a94
getabetterpic <dan@danandjensmith.com>
2016-06-24 18:48:22 -0400

    Change logger to send to stdout

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Log to stdout in production so the logs show in Puma logs
+  config.logger = Logger.new(STDOUT)
 end

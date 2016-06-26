38b570d0a4fd9ad549a9beebf54694847b7e3282
getabetterpic <dan@danandjensmith.com>
2016-06-26 16:22:55 -0400

    Set up rocket and image association

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,5 @@
 class Image < ActiveRecord::Base
   has_attached_file :image
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  belongs_to :imageable, polymorphic: true
 end
--- app/models/rocket.rb
+++ app/models/rocket.rb
@@ -2,6 +2,7 @@ class Rocket < ActiveRecord::Base
   validates :name, presence: true, uniqueness: { scope: :manufacturer }
   has_many :flights
   has_many :motors, through: :flights
+  has_many :images, as: :imageable
 
   def successful_flights
     flights.where(successful: true)

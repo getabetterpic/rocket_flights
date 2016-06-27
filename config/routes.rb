86396ad982d494bb479e40b91c06007a273e9db5
getabetterpic <dan@danandjensmith.com>
2016-06-27 18:03:32 -0400

    Just use jsonapi_resources route for all images endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,7 @@ Rails.application.routes.draw do
   jsonapi_resources :users, only: [:index]
   jsonapi_resources :user_flights
   jsonapi_resources :user_rockets
-  jsonapi_resources :images, except: [:create]
-  post 'images' => 'images#create'
+  jsonapi_resources :images
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
